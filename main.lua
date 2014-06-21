5269405d102961025a1b4ddd88d956aace38c4fd
Hugo Puhlmann <hugopuhlmann@gmail.com>
2014-06-21 00:40:58 -0300

    Refactoring game logic to use the new shape module.

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+sh = require "shapes"
+
 local function resetPads()
 	w, h = love.window.getDimensions()
 	local padw, padh = 30, 120
@@ -6,7 +8,7 @@ local function resetPads()
 	s = { 0.707, -0.707 }
 	padleft = { x = 0, y = py, w = padw, h = padh }
 	padright = { x = w - padw, y = py, w = padw, h = padh }
-	ball = { x = w/2, y = h/2, rad = 10, vx = s[math.random(2)], vy = s[math.random(2)], sc = 600 }
+	ball = { x = w/2 - 10, y = h/2 - 10, w = 20, h = 20, vx = s[math.random(2)], vy = s[math.random(2)], sc = 600 }
 end
 
 local function clamp(v, l, r)
@@ -38,27 +40,6 @@ local function handleJoystick(device, v, sc, dt)
 	end
 end
 
-local function pointInRectangle(r, p)
-	return p.x >= r.x and p.x <= r.x + r.w and p.y >= r.y and p.y <= r.y + r.h
-end
-
-local function anyInRect(rect, cpts) 
-	for k, p in ipairs(cpts) do
-		if pointInRectangle(rect, p) then
-			return true
-		end
-	end
-	return false
-end
-
-local function intersectSides(ball, pad)
-	return anyInRect(pad, { { x = ball.x + ball.rad, y = ball.y }, { x = ball.x - ball.rad, y = ball.y } })
-end
-
-local function intersectCaps(ball, pad)
-	return anyInRect(pad, { { x = ball.x, y = ball.y + ball.rad}, { x = ball.x, y = ball.y - ball.rad } })
-end
-
 local function setupInputMethods()
 	padHandlers = { function(y, spd, dt) return handleKeyboard(y, "down", "up", spd, dt) end,
 		function(y, spd, dt) return handleKeyboard(y, "r", "w", spd, dt) end }
@@ -83,29 +64,26 @@ function love.update(dt)
 	padright.y = clamp(padright.y, 0, h - padright.h)
 
 	local nx, ny = ball.x + ball.vx * ball.sc * dt, ball.y + ball.vy * ball.sc * dt
-	local nball = { x = nx, y = ny, rad = ball.rad }
+	local nball = { x = nx, y = ny, w = ball.w, h = ball.h }
 
-	if nx < ball.rad or nx > w - ball.rad then
+	if nx < ball.w or nx > w - ball.w then
 		resetPads()
-		if nx < ball.rad then
+		if nx < ball.w then
 			score[2] = score[2] + 1;
 		else
 			score[1] = score[1] + 1;
 		end
 		return
 	end
-	if intersectCaps(nball, padleft) or intersectCaps(nball, padright) then
-		ball.vx = -ball.vx
-		ball.vy = -ball.vy
-	elseif intersectSides(nball, padleft) or intersectSides(nball, padright) then
+	if #(sh.rectInRect(padleft, nball)) > 0 or #(sh.rectInRect(padright, nball)) > 0 then
 		ball.vx = -ball.vx
 	else
-		if nx < ball.rad or nx > w - ball.rad then
+		if nx < ball.w or nx > w - ball.w then
 			ball.vx = -ball.vx
 		else
 			ball.x = nx
 		end
-		if ny < ball.rad or ny > h - ball.rad then
+		if ny < ball.h or ny > h - ball.h then
 			ball.vy = -ball.vy
 		else
 			ball.y = ny
@@ -120,5 +98,5 @@ function love.draw()
 	love.graphics.print(sct[2], w/2 + f:getWidth(sct[2]), 10)
 	love.graphics.rectangle("fill", padleft.x, padleft.y, padleft.w, padleft.h)
 	love.graphics.rectangle("fill", padright.x, padright.y, padright.w, padright.h)
-	love.graphics.circle("fill", ball.x, ball.y, ball.rad, 30)
+	love.graphics.rectangle("fill", ball.x, ball.y, ball.w, ball.h)
 end
